4840eeb4782d057b6a943e1b0d31ee1f9265a204
Daniel Minichetti <daniel.minichetti@gmail.com>
2024-07-17 21:30:46 -0700

    Add files via upload

---
+++ Car Stats/src/practice.js
@@ -0,0 +1,34 @@
+const cars = [
+  {
+    model: "Honda Civic",
+    coloursByPopularity: ["black", "silver"],
+    speedStats: {
+      topSpeed: 140,
+      zeroToSixty: 8.5
+    }
+  },
+  {
+    model: "Tesla Model 3",
+    coloursByPopularity: ["red", "white"],
+    speedStats: {
+      topSpeed: 150,
+      zeroToSixty: 3.2
+    }
+  },
+  {
+    model: "Toyota Corolla",
+    speedStats: {
+      topSpeed: 120
+    },
+    coloursByPopularity: ["white", "black", "grey"]
+  },
+  {
+    model: "Ford Mustang",
+    speedStats: {
+      topSpeed: 165
+    },
+    coloursByPopularity: ["red", "blue", "black"]
+  }
+];
+
+export default cars;
